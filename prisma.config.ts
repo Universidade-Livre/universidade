41cf5f764cd7538ef4e978eac02edfce2273c973
nayetdet <jpmoreiracampos21@gmail.com>
2026-02-18 04:40:08 -0300

    fix: fixed prisma.config.ts for deployment reasons

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,10 +4,6 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } = process.env;
-if (!POSTGRES_HOST || !POSTGRES_DB || !POSTGRES_USER || !POSTGRES_PASSWORD) {
-  throw new Error("As variáveis de ambiente do PostgreSQL não foram definidas.");
-}
-
 export const datasourceUrl: string = `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/${POSTGRES_DB}`;
 export default defineConfig({
   schema: "prisma/schema.prisma",
