69054ef3282e1bab369281826334f977a634c011
nayetdet <jpmoreiracampos21@gmail.com>
2026-02-26 00:35:07 -0300

    feat: simplify datasource URL construction in prisma.config.ts and update Dockerfile to remove redundant prisma copy

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,17 +3,17 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
+const { POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } = process.env;
+const datasourceUrl: string | undefined = POSTGRES_HOST && POSTGRES_PORT && POSTGRES_DB && POSTGRES_USER && POSTGRES_PASSWORD
+  ? `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}`
+  : undefined;
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: ((): string | undefined => {
-      const { POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } = process.env;
-      return (POSTGRES_HOST && POSTGRES_PORT && POSTGRES_DB && POSTGRES_USER && POSTGRES_PASSWORD)
-        ? `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}`
-        : undefined;
-    })(),
+    url: datasourceUrl,
   },
 });
